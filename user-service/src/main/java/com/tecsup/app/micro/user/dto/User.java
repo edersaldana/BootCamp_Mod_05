73eb8c9a33faa0095fe3930187ee322eb89da0a9
edersaldana <eder.11.joel@gmail.com>
2025-10-08 17:26:49 -0500

    feat: Clase de estructura hexagonal para el user.

---
+++ user-service/src/main/java/com/tecsup/app/micro/user/dto/User.java
@@ -0,0 +1,17 @@
+package com.tecsup.app.micro.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+}
